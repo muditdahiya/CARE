a7759dd791696733011dac8f2777c429fd4bcab2
muditdahiya <mdtheroxtar@gmail.com>
2022-12-02 20:31:14 -0500

    solved back button, made files

---
+++ CARE/TabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarController.swift
+//  CARE
+//
+//  Created by Mudit Dahiya on 2022-12-02.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.navigationItem.hidesBackButton = true
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
